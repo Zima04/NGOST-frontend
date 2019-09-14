771ea2c7321d594841cd59ac2257bffcd428b944
Capedzillah <bernatskyav@gmail.com>
2019-09-14 21:27:07 +0300

    simple header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <VueHeader />
-    <div class="content">
+    <main class="content">
       <router-view></router-view>
-    </div>
+    </main>
     <VueFooter />
   </div>
 </template>
@@ -23,15 +23,11 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-  .content {
-    margin: 0 60px;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
   }
 </style>
