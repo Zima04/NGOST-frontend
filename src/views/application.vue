70ca7ebba168d9b54286361fd2603da6ecec6cc3
Capedzillah <bernatskyav@gmail.com>
2019-10-09 21:49:47 +0300

    updated router

--- src/views/application.vue
+++ src/views/application.vue
@@ -35,7 +35,7 @@ export default {
         },
         {
           name: 'Формирование требований',
-          linkTo: '/'
+          linkTo: '/app/create-demand'
         },
         {
           name: 'Список требований',
