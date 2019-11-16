01b80b219288d514d62b7ae80e1cac5c655dc700
Capedzillah <bernatskyav@gmail.com>
2019-11-16 19:32:57 +0300

    responsive

--- src/views/news.vue
+++ src/views/news.vue
@@ -160,7 +160,6 @@ $light_gray: lighten($black, 60%);
 	z-index: 1;
 	display: block;
 	background: $white;
-	min-width: 270px;
 	height: 470px;
 	@include box-shadow;
 	@include transitions;
@@ -267,10 +266,32 @@ $light_gray: lighten($black, 60%);
 	margin: 0 auto;
 	font-size: 14px;
 
+	@media only screen and (max-width: 479px) {
+		padding: 0.2rem;
+	}
+	@media (min-width: 481px) and (max-width: 767px) {
+		padding: 0.8rem;
+	}
+	@media (min-width: 768px) and (max-width: 1280px) {
+		padding: 1rem;
+	}
+
 	.column {
 		width: 25%;
 		padding: 1rem;
 		@include box-size;
+		@media only screen and (max-width: 479px) {
+			width: 100%;
+			padding: 0.2rem;
+		}
+		@media (min-width: 481px) and (max-width: 767px) {
+			width: 50%;
+			padding: 0.5rem;
+		}
+		@media (min-width: 768px) and (max-width: 1280px) {
+			width: 33%;
+			padding: 0.3rem;
+		}
 	}
 
 	.info {
